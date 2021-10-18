dd0827c476abd38a24405c28ebbf1c4593c3276f
shadowlucario <lucassm618@gmail.com>
2021-10-18 16:56:03 -0400

    start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <div class="container_form">
+      <div class="container grid-xs py-2">
+        <img class= "img-responsive img-logo" src="@/assets/shisui.png" alt="imagem de logo">
+        <form @submit.prevent="addtodo(todo)">
+          <div class="input-group">
+            <input type="text" v-model="todo.description" class="form-input" placeholder="novo todo">
+            <button class="btn btn-primary input-group-btn">Adicionar</button>
+          </div>
+          {{ todos }}
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return { 
+      todos: [],
+      todo: {checked: false } 
+    };
+  },
+  methods: {
+    addtodo(todo) {
+       todo.id = Date.now();
+       this.todos.push(todo);
+    }
   }
+  
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo {
+  max-width: 100px;
+  margin: 10% auto;
+}
+.home {
+  width: 100vw;
+  height: 100vh;
+}
+.container_form {
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  display: flex;
 }
 </style>
